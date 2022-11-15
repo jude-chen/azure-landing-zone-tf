8ec925d0f6846ce968f21bdee0ac2dd72fb81352
Jude Chen <judechen@microsoft.com>
2022-11-15 13:18:43 -0600

    added remote backend.

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  #   backend "azurerm" {
-  #     resource_group_name  = "terraform-bootstrap-rg"
-  #     storage_account_name = "judedemotfstatesa01"
-  #     container_name       = "jude-demo"
-  #     key                  = "bootstrap.terraform.tfstate"
-  #   }
+  backend "azurerm" {
+    resource_group_name  = "terraform-bootstrap-rg"
+    storage_account_name = "judechenlztfsa01"
+    container_name       = "tfstates"
+    key                  = "bootstrap.terraform.tfstate"
+  }
 }
